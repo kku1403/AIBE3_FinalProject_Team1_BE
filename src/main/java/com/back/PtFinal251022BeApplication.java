1ae0b0609ba1492d86002adda1764ada046a814f
Yoepee <kdy2975@naver.com>
2025-10-22 00:56:29 +0900

    init: 초기설정

---
+++ src/main/java/com/back/PtFinal251022BeApplication.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtFinal251022BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtFinal251022BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/PtFinal251022BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtFinal251022BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
