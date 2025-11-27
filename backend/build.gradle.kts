e10248ca4d91490c3801a4f0b6a3ddd9168d98b3
GeunYeong <jgy091411@gmail.com>
2025-11-27 09:16:38 +0900

    :heavy_plus_sign: chore : mcp-server 의존성 추가

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation("org.springframework.ai:spring-ai-openai")               // Embedding
     implementation("org.springframework.ai:spring-ai-rag")
     implementation("org.springframework.ai:spring-ai-starter-vector-store-mariadb")
+    implementation("org.springframework.ai:spring-ai-starter-mcp-server-webmvc")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
