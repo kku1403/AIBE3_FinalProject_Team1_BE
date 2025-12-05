71c6077c99f8f3f9a7b421e829ac2c6be54f1a9d
ehgk4245 <ehgkrk14@naver.com>
2025-12-05 11:14:44 +0900

    test: 테스트 sql 수정

--- backend/src/test/resources/sql/posts.sql
+++ backend/src/test/resources/sql/posts.sql
@@ -1,3 +1,7 @@
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE post;
+SET FOREIGN_KEY_CHECKS = 1;
+
 INSERT INTO post (id, created_at, modified_at, content, deposit, fee, is_banned, receive_method, return_address1, return_address2, return_method, title, author_id, category_id, embedding_status)
 VALUES
     -- 멤버 1이 작성한 3개의 포스트
@@ -9,6 +13,10 @@ VALUES
     (4, NOW(), NOW(), '노트북 대여', 50000, 10000, b'0', 'DELIVERY', '경기도 성남시', '분당동', 'DELIVERY', '노트북 빌려요', 2, 7, 'PENDING'),
     (5, NOW(), NOW(), '카메라 대여', 40000, 8000, b'0', 'DIRECT', '경기도 성남시', '분당동', 'DIRECT', '카메라 빌려요', 2, 8, 'DONE'),
     (6, NOW(), NOW(), '스마트폰/태블릿 대여', 30000, 7000, b'0', 'DELIVERY', '경기도 성남시', '분당동', 'DELIVERY', '태블릿 빌려요', 2, 9, 'PENDING');
+
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE post_region;
+SET FOREIGN_KEY_CHECKS = 1;
 
 INSERT INTO post_region (id, created_at, modified_at, post_id, region_id)
 VALUES
