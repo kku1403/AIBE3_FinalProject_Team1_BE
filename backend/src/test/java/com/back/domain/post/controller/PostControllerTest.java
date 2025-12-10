44a73244b765e28b5eed661dc4870b8ed96a5fa1
jjuchan <yjc7241@naver.com>
2025-12-10 09:33:14 +0900

    test: postvector remove

--- backend/src/test/java/com/back/domain/post/controller/PostControllerTest.java
+++ backend/src/test/java/com/back/domain/post/controller/PostControllerTest.java
@@ -1,8 +1,10 @@
 package com.back.domain.post.controller;
 
-import com.back.config.TestConfig;
-import com.back.domain.post.service.PostVectorService;
-import com.back.global.s3.S3Uploader;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,13 +20,8 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import com.back.config.TestConfig;
+import com.back.global.s3.S3Uploader;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -47,9 +44,6 @@ class PostControllerTest {
 
 	@MockitoBean
 	private S3Uploader s3Uploader;
-
-	@MockitoBean
-	private PostVectorService postVectorService;
 
 	@BeforeEach
 	void setup() {
